fbe080970cd37e81204d77c4a374d6dd72a631ac
Francois-Xavier Guillemette <francois.x.guillemette@gmail.com>
2011-04-12 13:41:24 -0400

    Added validation to Entry entity.

--- src/main/groovy/ca/code3/zeit/Entry.groovy
+++ src/main/groovy/ca/code3/zeit/Entry.groovy
@@ -1,13 +1,18 @@
 package ca.code3.zeit
 
+import javax.validation.*
+import javax.validation.constraints.*
+import org.hibernate.validator.constraints.*
+
+@ScriptAssert(lang="groovy", script="_this.tags.every{it}")
 class Entry {
     int id, rev /* for Hibernate */
 
-    User user
-    String comment
-    Date recordedOn
-    int duration
-    Set tags = []
+    @NotNull @Valid User user
+    @NotNull @Length(max=140) String comment
+    @NotNull @Past Date recordedOn
+    @Min(1L) int duration
+    @NotNull Set<String> tags = []
 
     /* return a copy */
     Date getRecordedOn () {
@@ -15,6 +20,8 @@ class Entry {
     }
     /* always copy then remove time component */
     void setRecordedOn (Date recordedOn) {
+        if (! recordedOn) throw new IllegalArgumentException("recordedOn may not be null")
+
         this.recordedOn = new Date(recordedOn.time)
         this.recordedOn.clearTime()
     }
--- src/test/groovy/ca/code3/zeit/EntryTest.groovy
+++ src/test/groovy/ca/code3/zeit/EntryTest.groovy
@@ -1,20 +1,104 @@
 package ca.code3.zeit
 
 import groovy.time.*
+import javax.validation.*
+
 import org.junit.*
 import static org.hamcrest.CoreMatchers.*
 import static org.junit.Assert.*
 
 class EntryTest {
-    def user
+    static validator
+    def validUser, testEntry
 
-    @Before void set_up () {
-        user = new User(email:"test@test.test", name:"test")
+    @BeforeClass static void set_up_once () {
+        def factory = Validation.buildDefaultValidatorFactory()
+        validator = factory.getValidator()
     }
 
-    @Test void it_should_hold_all_information () {
+    @Before void set_up () {
+        validUser = new User(email:"test@test.test", name:"test")
+        testEntry = new Entry(user: validUser, comment: "the comment.", recordedOn: new Date(), duration: 30, tags: ["tag-a", "tag-b"])
+    }
+
+    @Test void it_should_validate_its_user () {
+        testEntry.user = new User(email: null, name: null)
+        def violations = validator.validate(testEntry)
+        assertEquals 2, violations.size()
+
+        testEntry.user = null
+        violations = validator.validate(testEntry)
+        assertEquals 1, violations.size()
+    }
+
+    @Test void it_should_validate_comment_length () {
+        testEntry.comment = null
+        def violations = validator.validate(testEntry)
+        assertEquals 1, violations.size()
+
+        testEntry.comment = ""
+        violations = validator.validate(testEntry)
+        assertEquals 0, violations.size()
+
+        testEntry.comment = "a"*140
+        violations = validator.validate(testEntry)
+        assertEquals 0, violations.size()
+
+        testEntry.comment = "a"*141
+        violations = validator.validate(testEntry)
+        assertEquals 1, violations.size()
+    }
+
+    @Test void dates_should_not_be_in_the_future () {
+        testEntry.recordedOn = new Date()
+        def violations = validator.validate(testEntry)
+        assertEquals 0, violations.size()
+        
         use (TimeCategory) {
-            new Entry(user:user, comment:"comment...", recordedOn:new Date(), duration:30, tags: ["tag-a", "tag-b", "tag-c"])
+            testEntry.recordedOn = 1.day.ago
+            violations = validator.validate(testEntry)
+            assertEquals 0, violations.size()
+
+            testEntry.recordedOn = 1.day.from.now
+            violations = validator.validate(testEntry)
+            assertEquals 1, violations.size()
         }
+    }
+    
+    @Test(expected=IllegalArgumentException)
+    void it_should_not_allow_null_dates () {
+        testEntry.recordedOn = null
+    }
+
+    @Test void duration_should_not_be_less_than_1 () {
+        testEntry.duration = 1
+        def violations = validator.validate(testEntry)
+        assertEquals 0, violations.size()
+
+        [0, -1].each { invalidValue ->
+            testEntry.duration = invalidValue
+            violations = validator.validate(testEntry)
+            assertEquals 1, violations.size()
+        }
+    }
+
+    @Test void tag_set_should_not_be_null () {
+        testEntry.tags = null
+        def violations = validator.validate(testEntry)
+        assertEquals 1, violations.size()
+    }
+
+    @Test void tags_should_not_be_blank () {
+        testEntry.tags = ["not-blank", "not-blank"]
+        def violations = validator.validate(testEntry)
+        assertEquals 0, violations.size()
+
+        testEntry.tags = ["", "not-blank"]
+        violations = validator.validate(testEntry)
+        assertEquals 1, violations.size()
+
+        testEntry.tags = [null, "not-blank"]
+        violations = validator.validate(testEntry)
+        assertEquals 1, violations.size()
     }
 }
